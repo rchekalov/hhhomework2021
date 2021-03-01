8f283b7d9789897f57fbc89d6e45ea8484c6657c
Abdulov Azad <abdulov.azad@yandex.ru>
2021-03-01 14:23:11 +0300

    calculator complete

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,31 @@
-from decorators import cache_decorator
+# from decorators import cache_decorator
+
+operations = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '/': lambda x, y: x / y,
+    '*': lambda x, y: x * y,
+    '**': lambda x, y: x ** y,
+}
 
 def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    try:
+        return operations[operation](a, b)
+    except KeyError:
+        print('Неверная операция')
+    except ZeroDivisionError:
+        print('Деление на ноль')
 
+def read_int():
+    while True:
+        try:
+            return int(input('Введите целое число: '))
+        except ValueError:
+            print('Неверное целое число')
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    a = read_int()
+    b = read_int()
+    operation = input('Введите операцию (+, -, /, *, **): ').strip()
 
-    print('Результат: ', calculator(a, b, operation))
+    print('Результат:', calculator(a, b, operation))
