b4235508d63f21c8c6682498cba34539ea8075f7
Abdulov Azad <abdulov.azad@yandex.ru>
2021-03-02 00:01:14 +0300

    classes complete

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,19 @@
-class Car:
-    # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
-    # Поля должны задаваться через конструктор
-    pass
+from collections.abc import Iterable
 
-class Garage:
-    # Написать класс гаража Garage, у которого есть поле списка машин
-    # Поле должно задаваться через конструктор
-    # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
-    # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+class Garage(Iterable):
+    def __init__(self, cars = []):
+        self.cars = cars
+
+    def __iter__(self):
+        return self.cars.__iter__()
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        self.cars.pop(index)
