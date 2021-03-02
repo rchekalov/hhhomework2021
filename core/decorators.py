bba332c177e6f81fbb15f016d9e6799b8971de64
Sergey Makovkin <mackovckin.sergei@yandex.ru>
2021-03-02 23:00:59 +0300

    Commit #4

--- core/decorators.py
+++ core/decorators.py
@@ -2,8 +2,11 @@ from collections import defaultdict
 
 def cache_decorator(func):
     cache = {}
-    def inner_function(*args):
-        if cache.get(args) == None:
-            cache[args] = func(*args)
-        return cache[args]
+    def inner_function(*args, **kwargs):
+        sorted_kwargs = {key: kwargs[key] for key in sorted(kwargs.keys())}
+        if cache.get(str([args, sorted_kwargs])) == None:
+            cache[str([args, sorted_kwargs])] = func(*args, **kwargs)
+        else:
+            print("Cached")
+        return cache[str([args, sorted_kwargs])]
     return inner_function
