52fe0baf4ec88bc4410ce42e2c9647fb9f9251bd
Robert Gaptrakhmanov <irobert608@gmail.com>
2021-03-06 02:11:11 +0300

    Python hw

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,23 @@
 class Car:
-    # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
-    # Поля должны задаваться через конструктор
-    pass
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars):
+        self.cars = cars
+
+    def __iter__(self):
+        for car in self.cars:
+            yield car
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        self.cars.pop(index)
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,12 @@
+import random
+
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
+    for i in range(N):
+        yield random.randint(1, 100)
+
 
 # написать генераторное выражение, которое делает то же самое
+N = int(input())
+gen_random_val = (random.randint(1, 100) for _ in range(N))
