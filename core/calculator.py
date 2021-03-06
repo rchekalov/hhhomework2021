dabb30f0d87cd0d7ea84ed86568b655b30a971e2
Eduard Tikhomirov <tikhomirovcloud@icloud.com>
2021-03-07 00:37:58 +0300

    hw done

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,43 @@
 from decorators import cache_decorator
 
-def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+
+@cache_decorator
+def calculator(__a, __b, __operation):
+    if __operation == '+':
+        return a + b
+    elif __operation == '-':
+        return a - b
+    elif __operation == '/':
+        try:
+            return a / b
+        except ZeroDivisionError:
+            print('Cannot divide by 0')
+            exit()
+    elif __operation == '*':
+        return a * b
+    elif __operation == '**':
+        return a ** b
+
+
+def input_int():
+    while True:
+        try:
+            return int(input('Введите число: '))
+        except ValueError:
+            print('Error. Try again...')
+
+
+def input_operation():
+    while True:
+        i = input('Введите операцию: ').strip()
+        if i in ('+', '-', '/', '*', '**'):
+            return i
+        print('Must be "+, -, /, *, **"')
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    a = int(input_int())
+    b = int(input_int())
+    operation = input_operation()
 
     print('Результат: ', calculator(a, b, operation))
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,14 @@
+import random
+
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
-def gen(N):
-    pass
+def gen(n):
+    rand = []
+    for i in range(n):
+        rand.append(random.randint(1, 100))
+    yield rand
+
 
 # написать генераторное выражение, которое делает то же самое
+num = 10
+[random.randint(1, 100) for _ in range(num)]
