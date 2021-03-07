23f028549dd1a095260da19cb1e1c6042944db1e
golovanovya <golovanovya@gmail.com>
2021-03-07 15:23:55 +0300

    calculator
    classes
    
    collections
    
    generators
    
    decorators

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,30 @@
 from decorators import cache_decorator
 
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
-
+    if operation == '+':
+        return a + b
+    if operation == '-':
+        return a - b
+    if operation == '*':
+        return a * b
+    if operation == '/':
+        if b == 0:
+          raise ValueError("Can't divide by 0")
+        return a / b
+    if operation == '**':
+        return a ** b
+    raise ValueError('Undefined operation: {}'.format(operation))
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    try:
+        a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+        b = int(input('Введите число: '))
+        operation = input('Введите операцию ')
 
-    print('Результат: ', calculator(a, b, operation))
+        print('Результат: ', calculator(a, b, operation))
+    except ValueError as err:
+        print(err)
--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,31 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
-    pass
+    def __init__(self, manufacturer, model):
+        self.manufacturer = manufacturer
+        self.model = model
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars):
+        self.cars = cars
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        del self.cars[index]
+
+    def __iter__(self):
+        self.current = 0
+        return self
+
+    def __next__(self):
+        if self.current == len(self.cars):
+            raise StopIteration
+        car = self.cars[self.current]
+        self.current += 1
+        return car
