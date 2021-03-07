feef6e76ff800998f768b585db72d00f4816134e
Разиков Алексей <razikov88@yandex.ru>
2021-03-08 00:53:43 +0300

    completed

--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,13 @@
+from random import randint
+
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
+    for i in range(N):
+        yield randint(1, 100)
+
 
 # написать генераторное выражение, которое делает то же самое
+limit = 10
+generator_expression = (randint(1, 100) for i in range(limit))
+
