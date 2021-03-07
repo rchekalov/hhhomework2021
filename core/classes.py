5ac220a7b7815ca99b562185f916723e35fffc89
Ivan Ilin <geniusmov@yandex.ru>
2021-03-07 17:36:14 +0300

    feat: add homework

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,33 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
-    # Поля должны задаваться через конструктор
-    pass
+    # Поля должны задаваться через конструкто
+    
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+
+    def __init__(self, cars):
+        self.cars = cars
+        
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        self.cars.pop(index)
+
+    def __iter__(self):
+        self.index = -1
+        return self
+
+    def __next__(self):
+        if self.index == len(self.cars) - 1:
+            raise StopIteration
+        self.index +=1
+        return self.cars[self.index]
+        
--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,14 @@
-def cache_decorator():
+import time
+def cache_decorator(func):
+    cashe = {}
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+    def inner(*args):
+        if args in cashe:
+            return cashe[args]
+        else:
+            ans = func(*args)
+            cashe[args] = ans
+            return ans
+    return inner
