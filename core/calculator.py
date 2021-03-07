15cc86a1b20e53266fbab65a09cec88d38689204
Kirill Pozdnyakov <kiry-98@mail.ru>
2021-03-07 22:23:54 +0300

    Added homework

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,48 @@
 from decorators import cache_decorator
 
+class BadOperationException(Exception):
+    pass
+
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    a = int(a)
+    b = int(b)
+
+    if operation not in ("+", "-", "/", "*", "**"):
+        raise BadOperationException("Операция введена не верно")
+    
+    if operation == "+":
+        return a + b
+    elif operation == "-":
+        return a - b
+    elif operation == "/":
+        return a / b
+    elif operation == "*":
+        return a * b
+    return a ** b
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+        a = input("Введите первое число: ").strip()
+        b = input("Введите второе число: ").strip()
+        operation = input("Введите операцию (+, -, /, *, **): ").strip()
 
-    print('Результат: ', calculator(a, b, operation))
+        try:
+            print("Результат: ", calculator(a, b, operation))
+        except ValueError:
+            print("Число введено не верно")
+            continue
+        except BadOperationException as e:
+            print(f"{e}")
+            continue
+        except ZeroDivisionError:
+            print("Деление на 0")
+            continue
+
+        is_exit = input("Чтобы закончить введите q: ")
+        if is_exit == 'q':
+            break
--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,13 @@
-def cache_decorator():
+from functools import wraps
+
+def cache_decorator(func):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+    cache = {}
+    @wraps(func)
+    def inner(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return inner
