f4a545abffc482fe2826e70f2a586f1cd37f1a3f
Veprintsev Boris <45390259+BorisVeprintzev@users.noreply.github.com>
2021-03-09 19:27:58 +0300

    classes ok!

--- core/classes.py
+++ core/classes.py
@@ -1,6 +1,9 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
+    def __init__(self, mark, model):
+        self.mark = mark
+        self.model = model
     pass
 
 class Garage:
@@ -8,4 +11,10 @@ class Garage:
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
+    def __init__(self, car_list):
+        self.car_list = car_list
+    def add(self, new_car):
+        self.car_list.append(new_car)
+    def delete(self, n):
+        self.car_list.pop(n)
     pass
