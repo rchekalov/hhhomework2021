8e817fc2e267fbadac6491feddd397592adb37ad
Veprintsev Boris <45390259+BorisVeprintzev@users.noreply.github.com>
2021-03-10 17:04:12 +0300

    work done

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,29 @@
 from decorators import cache_decorator
 
+def bad_op():
+    raise Exception("Неверная операция")
+
+dict_operation = {
+    '+': lambda x, y: x + y,
+    '*': lambda x, y: x * y,
+    '-': lambda x, y: x - y,
+    '/': lambda x, y: x / y,
+    '**': lambda x, y: x ** y,
+}
+
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    print(dict_operation[operation](a, b))
+    a = dict_operation[operation](a, b)
+    return a
 
 
 if __name__ == '__main__':
     a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
     b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    operation = input('Введите операцию: ')
 
     print('Результат: ', calculator(a, b, operation))
