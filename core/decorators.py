b580a814499540941bfee44b615ff2b9b023d340
jeyxeon <jeyxxs@gmail.com>
2021-03-10 17:28:38 +0300

    First resolving

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,15 @@
-def cache_decorator():
+import functools
+
+def cache_decorator(func):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        cache_key = args + tuple(kwargs.items())
+        if cache_key not in wrapper.cache:
+            wrapper.cache[cache_key] = func(*args, **kwargs)
+        return wrapper.cache[cache_key]
+    wrapper.cache = dict()
+    return wrapper
+
