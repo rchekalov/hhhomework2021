9af480934f22aa2b6b70c9b6abaa4cf19f3ba4dc
Evgeniy <56478221+Evgeniy638@users.noreply.github.com>
2021-03-12 23:57:31 +0300

    added calculator, collections, decorators

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,14 @@
-def cache_decorator():
+def cache_decorator(f):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+
+    cache = {}
+
+    def g(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if not (key in cache):
+            cache[key] = f(*args, **kwargs)
+        return cache[key]
+    
+    return g
