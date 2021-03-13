211e39ae20f229ba8d75b61dfda22fa300f373ce
Dmitry Vasyanin <dvasyanin0@gmail.com>
2021-03-14 00:08:30 +0300

    solved the task 1

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,37 @@
 from decorators import cache_decorator
 
+operations = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '/': lambda x, y: x / y,
+    '*': lambda x, y: x * y,
+    '**': lambda x, y: x ** y,
+}
+
+
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    try:
+        return operations[operation](a, b)
+    except KeyError:
+        print('unknown operation')
+    except ZeroDivisionError:
+        print('division by zero')
+
+
+def reading_numbers():
+    while True:
+        try:
+            return int(input('enter a natural number: '))
+        except ValueError:
+            print('this number is not natural')
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    a = reading_numbers()  # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+    b = reading_numbers()
+    operation = input('Введите операцию').strip()
 
     print('Результат: ', calculator(a, b, operation))
