4ed3f584fdaa9e8727a52c88320053f6f98b2a8f
sokket <kn@typex.one>
2021-03-13 11:49:56 +0300

    Done

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,32 @@
 from decorators import cache_decorator
 
+@cache_decorator
 def calculator(a, b, operation):
     # Здесь нужно реализовать функцию,
     # которая реализует основные арифметические операции между числами: +, -, /, *, **.
     # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    ops = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '/': lambda a, b: a / b,
+        '*': lambda a, b: a * b,
+        '**': lambda a, b: a ** b
+    }
+    if operation in ops:
+        return ops[operation](a, b)
+    else:
+        raise ValueError
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    while True:
+        try:
+            a = int(input('Введите число: '))
+            b = int(input('Введите число: '))
+            operation = input('Введите операцию: ')
 
-    print('Результат: ', calculator(a, b, operation))
+            print('Результат: ', calculator(a, b, operation))
+        except ZeroDivisionError:
+            print('Делить на ноль нельзя')
+        except ValueError as err:
+            print('Ошибка ввода')
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,10 @@
+from random import randint
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
 def gen(N):
-    pass
+    for i in range(0, N):
+        yield randint(1, 100)
 
 # написать генераторное выражение, которое делает то же самое
+N = 10
+gen_expression = (randint(1, 100) for i in range(0, N))
