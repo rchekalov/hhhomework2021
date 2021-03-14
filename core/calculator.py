b34404da093b367b7864353338dc84e9074da804
Nikolai Malov <nmalov@fun.co>
2021-03-14 18:25:00 +0300

    Malov homework

--- core/calculator.py
+++ core/calculator.py
@@ -1,14 +1,28 @@
 from decorators import cache_decorator
 
+
+@cache_decorator
 def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    if operation == '+':
+        return a + b
+
+    elif operation == '-':
+        return a - b
+
+    elif operation == '/':
+        return a / b
+
+    elif operation == '*':
+        return a * b
+
+    elif operation == '**':
+        return a ** b
+    else:
+        print('Enter a valid operator, please run the program again.')
 
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
+    a = int(input('Введите число: '))  # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
     b = int(input('Введите число: '))
     operation = input('Введите операцию')
 
--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,25 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
-    pass
+    class Car:
+        def __init__(self, mark, make):
+            self.mark = mark
+            self.make = make
+
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars=list[Car]):
+        self.cars = cars
+
+    def __iter__(self):
+        return iter(self.cars)
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        self.cars.pop(index)
