5ef9ef6f9028b3c9d5868ca214ad99b4a2f3160b
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-16 14:18:27 +0500

    succes generators

--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,12 @@
+from random import randint
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
-def gen(N):
-    pass
 
+def gen(N):
+    count = N     
+    while count > 0:
+        yield randint(0, 100)
+        count -= 1
+            
 # написать генераторное выражение, которое делает то же самое
+N = 5
+gen2 = (randint(0, 100) for _ in range(N))
