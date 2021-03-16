1248f0b78660cdbe9b702b8a5a336b8d95f34846
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-16 14:02:22 +0500

    succes calculator

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,36 @@
 from decorators import cache_decorator
 
+@cache_decorator
 def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+    if operation == '+':
+        return a + b
+    elif operation == '-':
+        return a - b
+    elif operation == '/':
+        return a / b
+    elif operation == '*':
+        return a * b
+    elif operation == '**':
+        return a ** b
+    else:
+        return 'некорректный оператор'
 
+def GetValue():
+    while True:
+        sentence = input('Введите число: ')
+
+        try:
+            value = int(sentence)
+        # Если полученный ввод не число, будет вызвано исключение
+        except ValueError:
+            # Цикл будет повторяться до правильного ввода
+            print("Error! Это не число, попробуйте снова.")
+        else:
+            return value
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
+    a = GetValue()
+    b = GetValue()
+    operation = input('Введите операцию ')
 
     print('Результат: ', calculator(a, b, operation))
