f24414d579bb4e6b91ebc160208314b3937bc0cc
ifuncuran <ifuncuran@gmail.com>
2021-03-27 14:56:51 +0300

    calc collect decorat generat

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,13 @@
-def cache_decorator():
+def cache_decorator(func):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+
+    cache = {}
+
+    def inner(*args, **kwargs): 
+        key = str(args) + str(kwargs)
+        if not (key in cache)
+            cashe[key] = func(*args, **kwargs)
+        return cashe[key]
+    return inner
